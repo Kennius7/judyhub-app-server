31c4de945193eaee307d29a18794956db10733f8
Kennius7 <ogbogukenny@yahoo.com>
2025-01-30 14:13:17 +0100

    fix: postdata endpoint added

---
+++ api/postdata.js
@@ -0,0 +1,44 @@
+
+const { doc, updateDoc } = require("firebase/firestore");
+const { db } = require("../FirebaseConfig.js");
+
+require('dotenv').config();
+
+
+
+export default async function handler(req, res) {
+    console.log("Checking...");
+
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+
+    if (req.method === "OPTIONS") {
+        res.status(200).end();
+        console.log("Checking OPTIONS Method...", res.statusCode);
+        return;
+    }
+
+    // Signing In Block
+    if (req.method === "POST") {
+        try {
+            const { products } = req.body;
+            const docRef = doc(db, "judyhub-products", "products");
+            await updateDoc(docRef, products);
+            const message = `Successfully posted data`;
+            return res.status(200).json({ success: true, message: message });
+        } catch (error) {
+            console.log("Checking POST Method ERROR...", res.statusCode);
+            console.log(error.code);
+            return res.status(500).json({ error: `Error: ${error.message}` });
+        }
+    }
+
+    if (!['POST', 'GET', 'OPTIONS'].includes(req.method)) {
+        console.log("Checking NO METHOD SELECTED...", res.statusCode);
+        return res.status(405).json({ success: false, message: 'Method not allowed' });
+    }
+}
+
+
+

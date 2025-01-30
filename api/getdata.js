ad88c88b8ebc90b04fa64b47388214eb52d6fc44
Kennius7 <ogbogukenny@yahoo.com>
2025-01-30 16:42:31 +0100

    fix: getdata endpoint fix - 04

--- api/getdata.js
+++ api/getdata.js
@@ -1,24 +1,21 @@
-const { query, collection, where, getDocs } = require("firebase/firestore");
+const { collection, getDocs } = require("firebase/firestore");
 const { db } = require("../FirebaseConfig.js");
-const jwt = require("jsonwebtoken");
 require('dotenv').config();
 
 
 
-const shosanAppSecretKey = process.env.SHOSAN_APP_SECRET_KEY;
+// const admin = require("firebase-admin");
 
-const admin = require("firebase-admin");
-
-if (!admin.apps.length) {
-    admin.initializeApp({
-        credential: admin.credential.cert({
-            projectId: process.env.FIREBASE_PROJECT_ID,
-            clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-            privateKey: process.env.FIREBASE_SERVICE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-        }),
-        databaseURL: 'https://shosan-acodemia-app.firebaseio.com',
-    });
-}
+// if (!admin.apps.length) {
+//     admin.initializeApp({
+//         credential: admin.credential.cert({
+//             projectId: process.env.FIREBASE_PROJECT_ID,
+//             clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+//             privateKey: process.env.FIREBASE_SERVICE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+//         }),
+//         databaseURL: 'https://shosan-acodemia-app.firebaseio.com',
+//     });
+// }
 
 export default async function handler(req, res) {
     console.log("Checking...");
@@ -33,18 +30,7 @@ export default async function handler(req, res) {
         return;
     }
 
-    // Fetching User Data Block
     if (req.method === "GET") {
-        // const token = req.headers.authorization.split('Bearer ')[1];
-        // if (!token) { return res.status(401).send("Access Denied!") };
-
-        // const user = jwt.verify(token, shosanAppSecretKey, (err, user) => {
-        //     if (err) return res.status(403).send("Invalid Token!");
-        //     req.user = user;
-        //     const userData = req.user;
-        //     return userData;
-        // })
-
         try {
             const querySnapshot = await getDocs(collection(db, "judyhub-products"));
             const filteredData = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));

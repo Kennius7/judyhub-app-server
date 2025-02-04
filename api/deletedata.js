8b640a241dfc36c1e3771403cb1f6fe3ea77117e
Kennius7 <ogbogukenny@yahoo.com>
2025-02-04 17:43:51 +0100

    fix: deletedata endpoint fix - 05

--- api/deletedata.js
+++ api/deletedata.js
@@ -1,4 +1,4 @@
-const { deleteDoc, doc } = require("firebase/firestore");
+const { updateDoc, doc } = require("firebase/firestore");
 const { db } = require("../FirebaseConfig.js");
 require('dotenv').config();
 
@@ -26,16 +26,36 @@ export default async function handler(req, res) {
             }
 
             let { ids } = req.body;
+            let docId = "products";
 
-            if (!Array.isArray(ids)) { ids = [ids] }
+            // if (!Array.isArray(ids)) { ids = [ids] }
 
             if (!ids || !Array.isArray(ids) || ids.length === 0) {
                 console.log("Invalid or empty ID list");
                 return res.status(400).json({ success: false, message: "Invalid or empty ID list" });
             }
 
-            const deletePromises = ids.map(id => deleteDoc(doc(db, "judyhub-products", id.toString())));
-            await Promise.all(deletePromises);
+            // const deletePromises = ids.map(id => deleteDoc(doc(db, "judyhub-products", id.toString())));
+            // await Promise.all(deletePromises);
+
+            const docRef = doc(db, "judyhub-products", docId);
+            const docSnap = await getDoc(docRef);
+            if (!docSnap.exists()) {
+                console.log("Document not found");
+                return res.status(404).json({ success: false, message: "Document not found" });
+            }
+            const data = docSnap.data();
+            const allProducts = data.allProducts || [];
+
+            // const updatedProducts = allProducts.filter(product => product.id !== productId);
+            const updatedProducts = allProducts.filter(product => !ids.includes(product.id));
+            
+            if (updatedProducts.length === allProducts.length) {
+                console.log("Product not found in array");
+                return res.status(404).json({ success: false, message: "Product not found in array" });
+            }
+
+            await updateDoc(docRef, { allProducts: updatedProducts });
 
             console.log(`✅ Deleted product with IDs: ${ids}`);
             return res.status(200).json({ success: true, message: "Product(s) deleted successfully" });

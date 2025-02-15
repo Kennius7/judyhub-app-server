1f4c3a6d84016c9cb339c2420a62aac779a96e7a
Kennius7 <ogbogukenny@yahoo.com>
2025-02-15 20:40:16 +0100

    fix: update profile data endpoint - 01

--- api/updateprofile.js
+++ api/updateprofile.js
@@ -20,7 +20,7 @@ export default async function handler(req, res) {
     }
 
     // Signing In Block
-    if (req.method === "POST") {
+    if (req.method === "POST" && !req.body.apiType) {
         try {
             const { profileFormData } = req.body;
             
@@ -48,6 +48,42 @@ export default async function handler(req, res) {
             await updateDoc(docRef, { name: name, email: email, number: number, address: address });
 
             const message = `Successfully updated your profile data!`;
+            console.log(message);
+            return res.status(200).json({ success: true, message: message });
+        } catch (error) {
+            console.log("Checking POST Method ERROR...", res.statusCode, error.message);
+            return res.status(500).json({ error: `Error: ${error.message}` });
+
+        }
+    }
+
+    // Update Cart Block
+    if (req.method === "POST" && req.body.apiType === "UPDATECART") {
+        try {
+            if (!req.body) {
+                console.log("Request body is missing or invalid.");
+                return res.status(400).json({ success: false, message: "Request body is missing or invalid." });
+            }
+
+            const { email, cartData } = req.body;
+            let id = "";
+            let userData;
+            console.log("Email:>>>>", email);
+
+            const q = query(collection(db, "User_Data"), where("email", "==", email));
+            const querySnapshot = await getDocs(q);
+
+            if (!querySnapshot.empty) {
+                const doc = querySnapshot.docs[0];
+                id = doc.id;
+                userData = doc.data();
+            }
+            console.log("id:>>>", id, "User Data:>>>", userData);
+
+            const docRef = doc(db, "User_Data", id);
+            await updateDoc(docRef, { cartData: cartData });
+
+            const message = `Successfully updated your profile picture!`;
             console.log(message);
             return res.status(200).json({ success: true, message: message });
         } catch (error) {
